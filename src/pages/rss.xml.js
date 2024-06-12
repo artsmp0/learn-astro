ad86f090804852445518b0f1fb071a2090241dc2
沈孟平 <artsmp@126.com>
2024-06-12 18:27:41 +0800

    修复 rss 报错

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 export async function GET(context) {
+  const posts = await getCollection('posts')
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
